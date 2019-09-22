4861d69c95de83200da28c145cecf7838eba85de
Xuan Jin <>
2019-09-22 15:09:25 -0400

    test on README

---
+++ 1.two-sum.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+/*        int size = nums.size(); 
+        //int result[size]; 
+        vector<int> result;  
+        for(int i=0; i<size; i++){
+            for(int j=1; j<=size-1; j++) {
+                if(target==nums[i]+nums[j]){
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+            
+        }
+        return result; */
+
+        //Key is the number and value is its index in the vector.
+	unordered_map<int, int> hash;
+	vector<int> result;
+	for (int i = 0; i < nums.size(); i++) {
+		int numberToFind = target - nums[i];
+
+            //if numberToFind is found in map, return them
+		if (hash.find(numberToFind) != hash.end()) {
+                    //+1 because indices are NOT zero based
+			result.push_back(hash[numberToFind]);
+			result.push_back(i);			
+			return result;
+		}
+
+            //number was not found. Put it in the map.
+		hash[nums[i]] = i;
+	}
+	return result;
+    }
+};
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Leetcode
-Leetcode Problem List
+# Leetcode Problem Sheet
+
+| #  | Title           | Difficulty  | Progress    |
+| --- | --- | --- | --- |
+| 1  | Two Sum         | Easy        | Done        |
+| 7  | Reverse Integer | Easy        | Not Started |
+| 15 | 3Sum            | Medium      | Not Started |
+|
+|
+|
+|
+|
+|
